9512e91b46de22742486a672015bb4753d06168d
Ewan Spencer <ewan@thespencers.me.uk>
2023-09-02 17:40:16 +0100

    Add the working week 5 project

--- rpi_pico/Week_5/Week_5.md
+++ rpi_pico/Week_5/Week_5.md
@@ -1,3 +1,15 @@
 # Week 5
 
-mqtt
+This week I am going to be experimenting with sending messages to a publish/subscribe server.  This will enable me to send and recieve messages to and from the device.
+
+I decided to use the popular [MQTT](https://mqtt.org/) protocol as there are many libraries available for Pyton that implement the protocol.
+
+As I am using micropython, I researched the corret library to use, and found [umqtt.simple](https://mpython.readthedocs.io/en/master/library/mPython/umqtt.simple.html).
+
+Once this was installed on the device, I was able to use it to connect to a public MQTT broker.  I decided to use [test.mosquitto.org](https://test.mosquitto.org/) as it is free to use.
+
+Once connected, I could see messages arriving on the topic, so I am ready to move on to the next step.
+
+## Problems
+
+This was quite a difficult task to complete because I needed help getting the umqtt.simple library installed on the target device.  I had to be manually copy it onto the device.  I think I was packaging the application incorrectly, so I will come back to this at a future date to try and resolve properly.
--- rpi_pico/Week_5/pub.py
+++ rpi_pico/Week_5/pub.py
@@ -1,5 +1,7 @@
 # pub.py
 import time
+import network
+from rpi_pico.Week_4 import credentials
 from umqtt.simple import MQTTClient
 
 server="test.mosquitto.org"
@@ -9,23 +11,35 @@ password = "public"
 topic = "raspberry/mqtt"
 msg = b'{"msg":"hello"}'
 
-def connect():
+def connect_wifi():
+    #Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(credentials.ssid, credentials.password)
+    while wlan.isconnected() == False:
+        print('Waiting for connection...')
+        sleep(1)
+    return wlan
+
+def connect_mqtt():
     print('Connected to MQTT Broker "%s"' % (server))
     client = MQTTClient(ClientID, server, 1883, user, password)
     client.connect()
     return client
 
-def reconnect():
+def reconnect_mqtt(client):
     print('Failed to connect to MQTT broker, Reconnecting...' % (server))
     time.sleep(5)
     client.reconnect()
 
 
 def run():
+    wifi = connect_wifi()
+    
     try:
-        client = connect()
+        client = connect_mqtt()
     except OSError as e:
-        reconnect()
+        reconnect_mqtt(client)
 
     while True:
         print('send message %s on topic %s' % (msg, topic))
